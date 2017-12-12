c97ccc4bdf7496bd1d8ac31f64958dedf2ec7196
Jovem Aprendiz05 <jovem.aprendiz05@conssulti.com.br>
2017-12-12 14:43:08 -0200

    html sobre e contato

--- contato.php
+++ contato.php
@@ -1,7 +1,23 @@
 <form>
-	<label for="Nome Completo:">Nome Completo:<labe><input type="text" required=""><br>
-	<label for="E-mail:">E-mail:</label><input type="email" required=""><br>
-	<label for="Assunto:">Assunto:</label><input type="text" required=""><br>
-	<label for="Mensagem:">Mensagem:</label><textarea rows="10" cols="30" required=""></textarea>
+	<fieldset>
+		<label for="contact-name" >Nome Completo:</label>
+		<input type="text" required="" id="contact-name">
+	</fieldset>
+	<fieldset>
+		<label for="contact-email">E-mail:</label>
+		<input type="email" required="" id="contact-email">
+	</fieldset>
+	<fieldset>
+		<label for="contact-assunto">Assunto:</label>
+		<input type="text" required="" id="contact-assunto">
+	</fieldset>
+	<fieldset>
+		<label for="contact-message">Mensagem:</label>
+		<textarea rows="10" cols="30" required="" id="contact-message"></textarea>
+	</fieldset>
 	<button type="submit">Enviar</button>
 </form>
+<p>Outras formas de contatos:</p>
+<p>contato@gmail.com</p>
+<p>contato@hotmail.com</p>
+<p>telefone: 4002-8922</p>
---
+++ sobre.php
@@ -0,0 +1,22 @@
+<p>Nascemos para transformar nossos alunos em profissionais qualificados e competitivos. Somos uma escola focada</p>
+<!-- logo -->
+<img src="">
+<p>Mantemos parcerias com diversas instituições de encinos</p>
+
+<!-- Time -->
+<h4>Jose da silva</h4>
+<img src="">
+<P>preve comentario sobre ele</P>
+
+<h4>trabalho madereiro de ferreira </h4>
+<img src="">
+<P>preve comentario sobre ele</P>
+
+<h4>Galho pinheiro de oliveira</h4>
+<img src="">
+<P>preve comentario sobre ele</P>
+<!-- imagem e link dos parceiros -->
+<a href="" ><img src=""></a>
+<a href="" ><img src=""></a>
+<a href="" ><img src=""></a>
+<a href="" ><img src=""></a>
