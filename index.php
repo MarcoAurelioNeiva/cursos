7959d5a1502e3c52ede68ed11d6d3982670a82bc
Marco Neiva <marco_neiva@hotmail.com>
2017-12-15 15:55:08 -0200

    colocando cursos

--- index.php
+++ index.php
@@ -25,20 +25,189 @@
 	</nav>
 	<!-- fim do topnav -->
 	<!-- lista de cursos -->
-	<div class="cursos">
-		<figure>
-			<a href="" target="_blank">
-				<div class="img_curso">
-					<img src="rick.png" alt="imagem do curso">
-				</div>
-				<figcaption class="figcaption">
-					<h4>Título curso</h4>
-					<p>Descrição curta</p>
-					<p>Gratuito</p>
-					<a href="https://google.com">Página do instrutor</a>
-				</figcaption>
-			</a>
-		</figure>
+	<div class="conteudo_principal">
+		<div class="cursos">
+			<div>
+				<figure>
+					<a href="" target="_blank">
+						<div class="img_curso">
+							<img src="unity.jpg" alt="imagem do curso">
+						</div>
+						<figcaption class="figcaption">
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="https://google.com" alt="pagina intrutor">Página instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">
+						<div class=img_curso>
+							<img src="maya.png" alt="imagem do curso">
+						</div>
+						<figcaption class="figcaption">
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina intrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">
+						<div class=img_curso>
+							<img src="construct.jpg" alt="imagem do curso">
+						</div>
+						<figcaption class="figcaption">
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina intrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="summer.jpg" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="rick.png" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="morty.jpg" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="html.jpg" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="css.jpg" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="javascript.jpg" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="violao.jpg" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="piano.jpg" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+			<div>
+				<figure>
+					<a href="" target="_blank">		
+						<div class="img_curso">
+							<img src="bateria.jpg" alt="imagem do curso">
+						</div>
+						<figcaption>
+							<h4>Título curso</h4>
+							<p>Descrição curta</p>
+							<p>Gratuito</p>
+							<a href="" alt="pagina instrutor">pagina instru</a>
+						</figcaption>
+					</a>
+				</figure>
+			</div>
+		</div>
 	</div>
 	<!-- fim da lista de cursos -->
 	<!-- footer -->
@@ -52,5 +221,5 @@
 	<!-- fim do footer -->
 </body>
 </html>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="CSS/reset.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="CSS/reset.css">
