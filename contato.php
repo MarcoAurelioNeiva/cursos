74e9acb33e588fc50e8e5d87fc86f0b178d582bd
Jovem Aprendiz05 <jovem.aprendiz05@conssulti.com.br>
2018-01-23 16:28:07 -0200

    latest ver.

--- contato.php
+++ contato.php
@@ -1,23 +1,32 @@
-<form>
-	<fieldset>
-		<label for="contact-name" >Nome Completo:</label>
-		<input type="text" required="" id="contact-name">
-	</fieldset>
-	<fieldset>
-		<label for="contact-email">E-mail:</label>
-		<input type="email" required="" id="contact-email">
-	</fieldset>
-	<fieldset>
-		<label for="contact-assunto">Assunto:</label>
-		<input type="text" required="" id="contact-assunto">
-	</fieldset>
-	<fieldset>
-		<label for="contact-message">Mensagem:</label>
-		<textarea rows="10" cols="30" required="" id="contact-message"></textarea>
-	</fieldset>
-	<button type="submit">Enviar</button>
-</form>
-<p>Outras formas de contatos:</p>
-<p>E-mail: contato@gmail.com</p>
-<p>E-mail: contato@hotmail.com</p>
-<p>telefone: 4002-8922</p>
+<?php
+require_once("header.html");
+?>
+<section id="section">
+	<form>
+		<fieldset>
+			<label for="contact-name" >Nome Completo:</label>
+			<input type="text" required="" id="contact-name">
+		</fieldset>
+		<fieldset>
+			<label for="contact-email">E-mail:</label>
+			<input type="email" required="" id="contact-email">
+		</fieldset>
+		<fieldset>
+			<label for="contact-assunto">Assunto:</label>
+			<input type="text" required="" id="contact-assunto">
+		</fieldset>
+		<fieldset>
+			<label for="contact-message">Mensagem:</label>
+			<textarea rows="10" cols="30" required="" id="contact-message"></textarea>
+		</fieldset>
+		<button type="submit">Enviar</button>
+	</form>
+	<p>Outras formas de contatos:</p>
+	<p>E-mail: contato@gmail.com</p>
+	<p>E-mail: contato@hotmail.com</p>
+	<p>telefone: 4002-8922</p>
+</section>
+<?php
+require_once("footer.html");
+?>
+<link rel="stylesheet" type="text/css" href="CSS/contato.css">
--- index.php
+++ index.php
@@ -1,29 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Cursos</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-	<!-- topnav -->
-	<nav class="topnav">
-		<div><img src="pena.jpg"></div>
-		<div>
-			<ul>
-				<li><a href="">Inicial</a></li>
-				<li><a href="">Cursos</a></li>
-				<li><a href="contato.php">Contatos</a></li>
-			</ul>
-			<div class="pesquisa">
-				<form id="form" action="" method="post">                       
-					<input type="text" name="busca" placeholder="buscar">
-					<button type="submit" class="btn">Pesquisar</button>
-				</form>
-			</div>
-		</div>
-	</nav>
-	<!-- fim do topnav -->
+<?php
+	require_once("header.html");
+?>
 	<!-- lista de cursos -->
 	<div class="conteudo_principal">
 		<div class="cursos">
@@ -210,16 +187,7 @@
 		</div>
 	</div>
 	<!-- fim da lista de cursos -->
-	<!-- footer -->
-	<div class="footer">
-		<a href="" class="fa fa-facebook"></a>
-		<a href="" class="fa fa-twitter"></a>
-		<a href="" class="fa fa-linkedin"></a>
-		<a href="" class="fa fa-youtube"></a>
-		<p>Desenvolvido por Marco Neiva</p>
-	</div>
-	<!-- fim do footer -->
-</body>
-</html>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="CSS/reset.css">
+	<?php
+		require_once("footer.html");
+	?>
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
